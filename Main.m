9f115f78965629238605088624bbcaf2df427539
Chadwick Healy <chad@Chadwicks-MacBook-Pro.local>
2015-12-06 14:16:17 -0700

    Changed structure of main, again.

--- Main.m
+++ Main.m
@@ -1,4 +1,6 @@
 %% Main Function
+
+%% Initialize
 clear,clf;
 homeDir = cd;
 cd('./MatlabUtil')
@@ -12,7 +14,7 @@ cd(homeDir)
 taus = tauLog;
 
 %% Regress Surface Location vs. Decay Constant for Model
-
+% Initialize Parameters
 types = 0:4;%Linf, L1, deadzone of size 0.5
 degrees = 1:2;% for now, linear and quadratic fits
 dz = 5;%nm, Deadzone
@@ -68,53 +70,3 @@ for mm = 1:tauTypes;
     disp('Residuals, Rows = degree of fit, Col = Penalty Fxn')
     resid_tots
 end
-
-% Run with Poly fit
-% taus = tauPoly;
-% 
-% %% Regress Taus
-% % TRY DIFFERENT MODELS
-% types = 0:4;%Linf, L1, deadzone of size 0.5
-% % degrees = 1:2;% for now, linear and quadratic fits
-% degrees = 1;% for now, linear and quadratic fits
-% dz = 5;%nm
-% 
-% %% LINEAR LEAST SQUARES--"gold standard"
-% residlsq = zeros(length(labels),length(degrees));
-% 
-% for ii = 1:length(degrees)
-%     xtemp = [];
-%     for jj = 0:ii
-%         xtemp = [taus.^jj, xtemp];
-%     end
-%     b = regress(labels,xtemp);
-%     % Find Model
-%     ymodel = zeros(length(taus),1);
-%     for jj = 1:ii
-%         ymodel = taus.*(b(jj) + ymodel);
-%     end
-%     ymodel = ymodel + b(end);
-%     % Plot Results
-%     figure
-%     hold on
-%     plot(taus,labels,'b.','MarkerSize',25);
-%     [tausorted,ind] = sort(taus);
-%     plot(tausorted,ymodel(ind),'r','LineWidth',2);
-%     % Find Residuals
-%     residlsq(:,ii) = labels - ymodel;
-%     resid_totlsq(ii) = sum(abs(residlsq(:,ii)));
-% end
-% 
-% 
-% %% USING LP, DIFFERENT PENALTY FXNS
-% %Initialize
-% resids = zeros(length(taus),length(degrees),length(types));
-% resid_tots = zeros(length(degrees),length(types));
-% residsq_tots = zeros(length(degrees),length(types));
-% 
-% % Run through different fits
-% for ii = 1:length(degrees)
-%     for jj = 1:length(types)
-%         [~,~,resids(:,ii,jj),resid_tots(ii,jj),residsq_tot(ii,jj)] = polyregression(taus,labels,degrees(ii),types(jj),dz,1);
-%     end
-% end
